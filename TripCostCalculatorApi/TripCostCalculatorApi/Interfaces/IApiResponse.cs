04b53e14b33fc29d61f79c67c28519cfd1d92c18
Connor Wagner <connorawagner@icloud.com>
2024-06-18 17:56:56 -0600

    Set up health check endpoint

---
+++ TripCostCalculatorApi/TripCostCalculatorApi/Interfaces/IApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using TripCostCalculatorApi.Models;
+
+namespace TripCostCalculatorApi.Interfaces;
+
+public interface IApiResponse
+{
+    public HttpStatusCode Status { get; }
+    public IEnumerable<ReturnableException>? Errors { get; }
+}
+
+public interface IApiResponse<T> : IApiResponse
+{
+    public T Data { get; }
+}
