cde80799d250ba498233d0c292fc355b315892ea
Connor Wagner <connorawagner@icloud.com>
2024-06-18 18:32:19 -0600

    Unit tests for health check work

---
+++ TripCostCalculatorApi/TripCostCalculatorApi.Test.Unit/Configuration/KebabCaseParameterTransformerTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using TripCostCalculatorApi.Configuration;
+
+namespace TripCostCalculatorApi.Test.Unit.Configuration;
+
+public class KebabCaseParameterTransformerTests
+{
+    public class TransformOutboundTests
+    {
+        private readonly KebabCaseParameterTransformer subject = new();
+
+        [Fact]
+        public void TransformOutbound_Should_Convert_PascalCase_To_KebabCase()
+        {
+            var input = "MultiPartControllerName";
+            var expected = "multi-part-controller-name";
+
+            var result = subject.TransformOutbound(input);
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
