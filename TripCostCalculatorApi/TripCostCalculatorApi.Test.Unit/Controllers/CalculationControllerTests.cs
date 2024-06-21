7de81548a123f4ae1905a76d5026057278c89876
Connor Wagner <connorawagner@icloud.com>
2024-06-21 08:15:27 -0600

    Unit test CalculationController

--- TripCostCalculatorApi/TripCostCalculatorApi.Test.Unit/Controllers/CalculationControllerTests.cs
+++ TripCostCalculatorApi/TripCostCalculatorApi.Test.Unit/Controllers/CalculationControllerTests.cs
@@ -1,7 +1,9 @@
-﻿using FluentAssertions;
+﻿using System.Net;
+using FluentAssertions;
 using Moq;
 using TripCostCalculatorApi.Controllers;
 using TripCostCalculatorApi.Domain.Interfaces;
+using TripCostCalculatorApi.Domain.Models;
 
 namespace TripCostCalculatorApi.Test.Unit.Controllers;
 
@@ -16,13 +18,37 @@ public class CalculationControllerTests
         public BalanceCostsTests()
         {
             mockCostBalancer = new();
+
             controller = new(mockCostBalancer.Object);
         }
 
         [Fact]
-        public void Should_Return_ApiResponse()
+        public void Should_Return_Balanced_Costs()
         {
-            false.Should().BeTrue();
+            BalancedCosts expectedResult = new()
+            {
+                OwedMoney = [
+                    new() {
+                        Recipient = new() {
+                            Name = "Member 1",
+                            Spent = 12.34M
+                        },
+                        Giver = new() {
+                            Name = "Member 2",
+                            Spent = 12.34M
+                        },
+                        Amount = 0M
+                    }
+                ]
+            };
+            mockCostBalancer
+                .Setup(m => m.BalanceCosts(It.IsAny<IEnumerable<TripMember>>()))
+                .Returns(expectedResult);
+
+            IEnumerable<TripMember> tripMembers = expectedResult.OwedMoney.SelectMany<OwedMoney, TripMember>(r => [r.Recipient, r.Giver]);
+            var result = controller.BalanceCosts(tripMembers);
+            result.Status.ShouldBeEquivalentTo(HttpStatusCode.OK);
+            result.Data.ShouldBeEquivalentTo(expectedResult);
         }
     }
 }
--- TripCostCalculatorApi/TripCostCalculatorApi/Controllers/CalculationController.cs
+++ TripCostCalculatorApi/TripCostCalculatorApi/Controllers/CalculationController.cs
@@ -14,7 +14,7 @@ public class CalculationController(ICostBalancerService costBalancerService) : C
     [HttpPost] // This would be a great candidate for the proposed QUERY method
     [Route("balance-costs")]
     [ProducesResponseType(typeof(IApiResponse<BalancedCosts>), (int)HttpStatusCode.OK)]
-    public IApiResponse<BalancedCosts> BalanceCosts([FromBody] TripMember[] tripMembers)
+    public IApiResponse<BalancedCosts> BalanceCosts([FromBody] IEnumerable<TripMember> tripMembers)
     {
         var result = costBalancerService.BalanceCosts(tripMembers);
 
